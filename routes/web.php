67b166e25be9ca98ab9bf71bdae590ee9c913eef
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-11-06 13:37:14 -0800

    cagajo

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ListaAnimales',function(){
+    $arm = DB::table('arminos')->get();
+    $glot = DB::table('gloton')->get();
+    $puercoS = DB::table('puercoSpin')->get();
+    $we = DB::table('buey')->get();
+    $pooh = DB::table('oso')->get();
+    $osoH = DB::table('osoHormiguero')->get();
+    
+return $arm . $glot . $puercoS . $we . $pooh . $osoH; })->middleware('grupoNo1');;
 
+Route::get('animales/{$id}', function($id){
+  $a=App\armino::find($id)->get($id);
+  $g=App\gloton::find($id)->get($id);
+  $p=App\puercoSpin::find($id)->get($id);
+  $w=App\buey::find($id)->get($id);
+  $p=App\oso::find($id)->get($id);
+  $o=App\osoHormiguero::find($id)->get($id);
+  return $a . $g . $p . $w . $p . $o ;
+})->middleware('grupoNo1');;
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
